1e864463e56d4abd3a9c82fc027390cfc0be1efc
Jessie Taylor <b3457xm0d3@gmail.com>
2016-08-18 14:56:00 -0400

    refactoring

--- App.jsx
+++ App.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, compose } from 'redux';
 import reduxReset from 'redux-reset';
 import { Provider } from 'react-redux';
+import undoable, { distinctState } from 'redux-undo';
 
 import reducer from './reducers';
 import PianoRoll from './components/PianoRoll.jsx';
 
+const undoableReducer = undoable(reducer, { filter: distinctState() });
 const enHanceCreateStore = compose(reduxReset())(createStore);
-const store = enHanceCreateStore(reducer, window.devToolsExtension && window.devToolsExtension());
+const store = enHanceCreateStore(undoableReducer, window.devToolsExtension && window.devToolsExtension());
 
 function App() {
   return (
--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,43 @@
-import { combineReducers } from 'redux';
-import notes from './sequencer';
+import constants from '../constants';
+import { createReducer } from '../utils';
 
-export default combineReducers({
-  sequencer
-});
+const { noteActionNames } = constants;
+
+function addNote(state, { note }) {
+  return state.concat([note]);
+}
+
+function removeNotes(state, {ids}) {
+  return state.filter((note, index) => ids.indexOf(index) === -1);
+}
+
+function moveNotes(state, {ids, beats, tones}) {
+  return state.map((note, index) => {
+    if (ids.indexOf(index) === -1) {
+      return note;
+    }
+    let newStart = note.start + beats;
+    if (newStart < 0) {
+      newStart = 0;
+    }
+    let newTone = note.tone + tones;
+    if (newTone < 0) {
+      newTone = 0;
+    }
+    return {
+      ...note,
+      tone: newTone,
+      start: newStart
+    };
+  });
+}
+
+const actionProcessors = {
+  [noteActionNames.ADD_NOTE]: addNote,
+  [noteActionNames.REMOVE_NOTES]: removeNotes,
+  [noteActionNames.MOVE_NOTES] : moveNotes
+};
+
+const initialState = [];
+
+export default createReducer(actionProcessors, initialState);
