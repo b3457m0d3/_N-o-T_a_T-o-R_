375b44ae2c6841b1a57feca5cee49cc61ef48105
Jessie Taylor <b3457xm0d3@gmail.com>
2016-08-18 12:33:52 -0400

    cleanup

--- App.jsx
+++ App.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reduxReset from 'redux-reset';
 import { Provider } from 'react-redux';
 
 import reducer from './reducers';
 import PianoRoll from './components/PianoRoll.jsx';
-var compose = function () {
-  var fns = arguments;
 
-  return function (result) {
-    for (var i = fns.length - 1; i > -1; i--) {
-      result = fns[i].call(this, result);
-    }
-
-    return result;
-  };
-};
-const enHanceCreateStore = compose(
-    //applyMiddleware(...),
-    reduxReset()  // Will use 'RESET' as default action.type to trigger reset
-  )(createStore);
-const store = enHanceCreateStore(reducer,window.devToolsExtension && window.devToolsExtension());
+const enHanceCreateStore = compose(reduxReset())(createStore);
+const store = enHanceCreateStore(reducer, window.devToolsExtension && window.devToolsExtension());
 
 function App() {
   return (
