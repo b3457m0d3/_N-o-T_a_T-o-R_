55b2dda88d058c5efd36304c285025a73fcccb55
Jessie Taylor <b3457xm0d3@gmail.com>
2016-08-18 20:34:49 -0400

    initial setup

--- App.jsx
+++ App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose } from 'redux';
+import { createStore, compose, combineReducers } from 'redux';
+import { ReduxRouter, routerStateReducer, reduxReactRouter, push } from 'redux-router';
 import reduxReset from 'redux-reset';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
+import { createHistory } from 'history';
+
 
 import reducer from './reducers';
 import PianoRoll from './components/PianoRoll.jsx';
+
+@connect((state) => ({}))
 
 const enHanceCreateStore = compose(reduxReset())(createStore);
 const store = enHanceCreateStore(reducer, window.devToolsExtension && window.devToolsExtension());
